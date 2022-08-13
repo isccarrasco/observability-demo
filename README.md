b7599a050eb4f4867c56cc129bdef8bda2a599be
isccarrasco <mario.carrasco@gmail.com>
2022-08-13 12:21:16 -0500

    Adding configuration files
    Signed-off-by: isccarrasco <mario.carrasco@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # observability-demo
+
 Demo for Observability with FastAPI and Prometheus
+
+```shell
+docker run \
+  -p 9090:9090 \
+  -v /PATH/TO/prometheus.yml:/etc/prometheus/prometheus.yml \
+  prom/prometheus
+```
+
+Run the FastAPI application
+
+```shell
+uvicorn app:app --reload --host 0.0.0.0 
+```
+
+host parameter is required to allow access from the dockerized prometheus 
--- app.py
+++ app.py
@@ -1,6 +1,25 @@
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator, metrics
 
 app = FastAPI()
+
+Instrumentator().add(
+    metrics.request_size(
+        should_include_handler=True,
+        should_include_method=False,
+        should_include_status=True,
+        metric_namespace="a",
+        metric_subsystem="b",
+    )
+).add(
+    metrics.response_size(
+        should_include_handler=True,
+        should_include_method=False,
+        should_include_status=True,
+        metric_namespace="namespace",
+        metric_subsystem="subsystem",
+    )
+).instrument(app).expose(app)
 
 
 @app.get("/")
